ace72f8b679961e7e48eb4a647dcdc9542cbe2ae
hadoop <hadoop@localhost>
2017-02-04 22:27:43 -0500

    1

---
+++ src/org/apache/hadoop/Query1.java
@@ -0,0 +1,67 @@
+package org.apache.hadoop;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Query1 {
+	public static class Q1Mapper extends Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				String[] s = word.toString().split(",");
+				if(Integer.valueOf(s[3])>=2&&Integer.valueOf(s[3])<=6){					
+					context.write(word, one);
+				}
+			}
+		}
+	}
+
+	public static class Q1Reducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		if (args.length != 2) {
+			System.err.println("Usage: Query-1 <HDFS input file> <HDFS output file>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "Query-1");
+		job.setJarByClass(Query1.class);
+		job.setMapperClass(Q1Mapper.class);
+//		job.setCombinerClass(Q2Reducer.class);
+//		job.setReducerClass(Q2Reducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setNumReduceTasks(2);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
